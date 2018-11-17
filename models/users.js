81679f4a1eee8a6ab359ccd10db951810f902101
Abeer Ishtiaq <mabeer@hotmail.com>
2018-11-17 09:14:36 -0500

    cleaned up code. removed all references to example

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    phone: DataTypes.INTEGER,
+    phone: DataTypes.NUMBER,
     username: DataTypes.STRING,
     password: DataTypes.STRING,
   });
--- public/js/index.js
+++ public/js/index.js
@@ -29,51 +29,15 @@ var API = {
       type: "POST",
       data: JSON.stringify(user)
     });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
 var handleFormSubmit = function(event) {
   event.preventDefault();
-
-  
-  console.log ("i am here");
 
   var user = {
     first_name: $first_name.val().trim(),
@@ -91,24 +55,12 @@ var handleFormSubmit = function(event) {
       $mob_no.val("");
       $username.val("");
       $password.val("");
+      window.location.href="/signin";
     }
   });
 
-  // $exampleText.val("");
-  // $exampleDescription.val("");
 };
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
 
 var handleLogin = function (){
  event.preventDefault();
@@ -130,4 +82,3 @@ var handleLogin = function (){
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $loginBtn.on("click", handleLogin);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
