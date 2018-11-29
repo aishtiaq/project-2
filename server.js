1f465df29af0b150f3b916423463bb3efe6dc391
Abeer Ishtiaq <mabeer@hotmail.com>
2018-11-28 19:26:43 -0500

    changes

--- server.js
+++ server.js
@@ -68,9 +68,7 @@ io.on("connection", function (socket) {
     socket.join(room);
     console.log("user joined room: " + room);
   });
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
+ 
   var room_name = socket.request.headers.referer.split("=")[2];
   socket.on("chat message", function (msg) {
     socket.join(room_name);
@@ -88,7 +86,57 @@ io.on("connection", function (socket) {
 		// echo globally that this client has left
 		socket.broadcast.emit('update message', 'SERVER', socket.username + ' has disconnected');
 		socket.leave(socket.room);
-	});
+  });
+
+  function log() {
+    var array = ['Message from server:'];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+  
+  socket.on('message', function(message) {
+    log('Client said: ', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('create or join', function(room) {
+    log('Received request to create or join room ' + room);
+
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (numClients === 0) {
+      socket.join(room);
+      log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+
+    } else if (numClients === 1) {
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('ipaddr', function() {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach(function(details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
+  });
+
+  socket.on('bye', function(){
+    console.log('received bye');
+  });
 });
 
 var syncOptions = { force: false };
